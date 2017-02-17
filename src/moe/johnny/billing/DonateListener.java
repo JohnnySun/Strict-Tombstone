92605fc697a1ca2867bf27779e9ebb1f6bc7a41e
JohnnySun <bmy001@gmail.com>
2017-02-17 18:05:39 +0800

    Change package name to moe.johnny.tombstone.

---
+++ src/moe/johnny/billing/DonateListener.java
@@ -0,0 +1,12 @@
+package moe.johnny.billing;
+
+import com.android.vending.billing.IInAppBillingService;
+
+/**
+ * Created by thom on 15/10/12.
+ */
+public interface DonateListener {
+    void onAvailable(IInAppBillingService service);
+
+    void onUnavailable(IInAppBillingService service);
+}
---
+++ src/moe/johnny/tombstone/ui/PreventList.java
@@ -0,0 +1,33 @@
+package moe.johnny.tombstone.ui;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import moe.johnny.tombstone.R;
+
+/**
+ * Created by thom on 16/2/17.
+ */
+public class PreventList extends PreventFragment {
+
+    @Override
+    protected Set<String> getPackageNames(PreventActivity activity) {
+        return new TreeSet<String>(activity.getPreventPackages().keySet());
+    }
+
+    @Override
+    protected int getQueryHint() {
+        return R.string.query_hint;
+    }
+
+    @Override
+    protected String getDefaultQuery() {
+        return null;
+    }
+
+    @Override
+    protected boolean canSelectAll() {
+        return true;
+    }
+
+}
